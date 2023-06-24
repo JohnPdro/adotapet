44503183d8a34b26cf463903dd8ca7208148c26a
João Pedro <112526011+JohnPdro@users.noreply.github.com>
2023-06-24 12:07:47 -0300

    Criando projeto com bootstrap

---
+++ inc/cabecalho.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AdotaPet</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+  </head>
+</head>
+<body>
+    <header class="container-fluid">
+        <div class="row">
+            <div class="col">
+                <img src="../img/logo.png" alt="">
+            </div>
+        </div>
+    </header>
+</body>
+</html>
