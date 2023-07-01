02bd2e820aa1cd719b5246f676e6ac5f12214170
João Pedro <112526011+JohnPdro@users.noreply.github.com>
2023-06-30 21:09:46 -0300

    Linkando paginas

--- inc/cabecalho-ong.php
+++ inc/cabecalho-ong.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>AdotaPet</title>
-    <link rel="stylesheet" href="../style.css">
+    <link rel="stylesheet" href="../css/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <link rel="shortcut icon" href="../img/favicon-16x16.png" type="image/x-icon">
@@ -23,7 +23,6 @@
                 <a href="../paginas/home.php" class="cor-btn-cabecalho m-1" role="button">Pets</a>
                 <a href="../paginas/abrigos.php" class="cor-btn-cabecalho m-1" role="button">Abrigos</a>
                 <a href="../paginas/cadastro.php" class="cor-btn-cabecalho m-1" role="button">Cadastre sua Ong</a>
-                <a href="../paginas/login.php" class="cor-btn-cabecalho m-1" role="button">Login</a>
             </div>
         </div>
     </header>
