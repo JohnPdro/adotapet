808ee13827198be9512a35701e05dbf3e752b1dc
João Pedro <112526011+JohnPdro@users.noreply.github.com>
2023-07-13 22:03:26 -0300

    Criando menu de ongs

---
+++ paginas/menu-interacao-ong.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="../css/style.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+  <link rel="shortcut icon" href="../img/favicon-16x16.png" type="image/x-icon">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Fredoka&display=swap" rel="stylesheet">
+</head>
+
+<body class="fundo-body">
+  <main class="">
+    <div class="container-fluid">
+      <div class="row pading-btn-menu">
+        <div class="col-4 m-auto">
+          <a href=""><button class="btn-menu-ong"><img src="../img/icon-home.png" alt=""><br>Home</button></a>
+          <a href=""><button class="btn-menu-ong"><img src="../img/icon-lista.png" alt=""><br>Lista de Pets</button></a>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-4 m-auto">
+          <a href=""><button class="btn-menu-ong"><img src="../img/icon-adicionar.png" alt=""><br>Cadastrar Pet</button></a>
+          <a href=""><button class="btn-menu-ong"><img src="../img/icon-sair.png" alt=""><br>Sair</button></a>
+        </div>
+      </div>
+    </div>
+  </main>
+
+  <footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+  </footer>
+</body>
+
+</html>
