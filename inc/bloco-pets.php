b0d608046503f409663a2d76a9c4b6fd14957b7d
João Pedro <112526011+JohnPdro@users.noreply.github.com>
2023-07-23 20:25:30 -0300

    Alterações nas aparencias

--- inc/bloco-pets.php
+++ inc/bloco-pets.php
@@ -10,9 +10,9 @@ include "../inc/conexao.php";
             <div class="card p-2" style="width: 20rem;">
                 <img src="../img/foto-gato.jpg" class="card-img-top" alt="...">
                 <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="cor-btn-blocopets" data-bs-toggle="modal" data-bs-target="#exampleModal">Go somewhere</a>
+                    <h5 class="card-title">Nome</h5>
+                    <p class="card-text">Informações</p>
+                    <a href="#" class="cor-btn-blocopets" data-bs-toggle="modal" data-bs-target="#exampleModal">Mais do Pet</a>
                     <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                         <div class="modal-dialog">
                             <div class="modal-content">
@@ -25,7 +25,6 @@ include "../inc/conexao.php";
                                 </div>
                                 <div class="modal-footer">
                                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                    <button type="button" class="btn btn-primary">Save changes</button>
                                 </div>
                             </div>
                         </div>
@@ -37,31 +36,81 @@ include "../inc/conexao.php";
 
         <div class="col m-2">
             <div class="card p-2" style="width: 20rem;">
-                <img src="../img/foto-cachorro.jpg" class="card-img-top" alt="...">
+                <img src="../img/foto-cachorro2.jpg" class="card-img-top" alt="...">
                 <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="cor-btn-blocopets">Go somewhere</a>
+                    <h5 class="card-title">Nome</h5>
+                    <p class="card-text">Informações</p>
+                    <a href="#" class="cor-btn-blocopets" data-bs-toggle="modal" data-bs-target="#exampleModal">Mais do Pet</a>
+                    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    ...
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
+
         <div class="col m-2">
             <div class="card p-2" style="width: 20rem;">
                 <img src="../img/foto-gato2.jpg" class="card-img-top" alt="...">
                 <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="cor-btn-blocopets">Go somewhere</a>
+                    <h5 class="card-title">Nome</h5>
+                    <p class="card-text">Informações</p>
+                    <a href="#" class="cor-btn-blocopets" data-bs-toggle="modal" data-bs-target="#exampleModal">Mais do Pet</a>
+                    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    ...
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
+
         <div class="col m-2">
             <div class="card p-2" style="width: 20rem;">
-                <img src="../img/foto-cachorro2.jpg" class="card-img-top" alt="...">
+                <img src="../img/foto-cachorro.jpg" class="card-img-top" alt="...">
                 <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="cor-btn-blocopets">Go somewhere</a>
+                    <h5 class="card-title">Nome</h5>
+                    <p class="card-text">Informações</p>
+                    <a href="#" class="cor-btn-blocopets" data-bs-toggle="modal" data-bs-target="#exampleModal">Mais do Pet</a>
+                    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    ...
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
--- paginas-ong/pets-cadastrados-ong.php
+++ paginas-ong/pets-cadastrados-ong.php
@@ -13,45 +13,48 @@ include "../inc/conexao.php";
 <h1 class=" ps-5 pb-5 pt-3 fonte-tema">Tabela de Pets Cadastrados</h1>
 
 <div class="container fonte-fredoka">
-  <table class="table fonte-tema">
-    <tr>
-      <td>Código</td>
-      <td>Especie</td>
-      <td>Nome</td>
-      <td>Sexo</td>
-      <td>Tamanho</td>
-      <td>Idade</td>
-      <td>Cidade</td>
-      <td>Estado</td>
-      <td>Sobre o Pet</td>
-      <td>Ações</td>
-    </tr>
+  </table>
+  <table class="table table-striped fonte-tema">
+    <thead>
+      <tr>
+        <th scope="col">Código</th>
+        <th scope="col">Especie</th>
+        <th scope="col">Nome</th>
+        <th scope="col">Sexo</th>
+        <th scope="col">Tamanho</th>
+        <th scope="col">Idade</th>
+        <th scope="col">Cidade</th>
+        <th scope="col">Estado</th>
+        <th scope="col">Sobre o Pet</th>
+        <th scope="col">Ações</th>
+      </tr>
+    </thead>
     <?php
     $sql = "select * from animais";
     $todos_registros = mysqli_query($conexao, $sql);
     while ($um_registro = mysqli_fetch_assoc($todos_registros)) :
     ?>
-      <tr>
-        <td> <?php echo $um_registro['id_animal']; ?></td>
-        <td> <?php echo $um_registro['especie']; ?></td>
-        <td> <?php echo $um_registro['nome']; ?></td>
-        <td> <?php echo $um_registro['sexo']; ?></td>
-        <td> <?php echo $um_registro['tamanho']; ?></td>
-        <td> <?php echo $um_registro['idade']; ?></td>
-        <td> <?php echo $um_registro['cidade']; ?></td>
-        <td> <?php echo $um_registro['estado']; ?></td>
-        <td> <?php echo $um_registro['sobre_pet']; ?></td>
-        <td>
-          <a class="p-2" href="../crud/visualizar.php?id=<?php echo $um_registro['id_animal']; ?>"><img src="../img/icon-visualizar.png" width="30" alt="Icone de visualizar" title="Visualizar"></a>
-          <a class="p-2" href="../crud/editar.php?id=<?php echo $um_registro['id_animal']; ?>"><img src="../img/icon-editar.png" width="30" alt="Icone de editar" title="Editar"></a>
-          <a class="p-2" href="../crud/deletar.php?id=<?php echo $um_registro['id_animal']; ?>"><img src="../img/icon-excluir.png" width="30" alt="Icone de excluir" title="Excluir"></a>
-        </td>
-      </tr>
-    <?php
-    endwhile;
-    ?>
-
+      <tbody>
+        <tr>
+          <td><?php echo $um_registro['id_animal']; ?></td>
+          <td><?php echo $um_registro['especie']; ?></td>
+          <td><?php echo $um_registro['nome']; ?></td>
+          <td><?php echo $um_registro['sexo']; ?></td>
+          <td><?php echo $um_registro['tamanho']; ?></td>
+          <td><?php echo $um_registro['idade']; ?></td>
+          <td><?php echo $um_registro['cidade']; ?></td>
+          <td><?php echo $um_registro['estado']; ?></td>
+          <td><?php echo $um_registro['sobre_pet']; ?></td>
+          <td><a class="p-2" href="../crud/visualizar.php?id=<?php echo $um_registro['id_animal']; ?>"><img src="../img/icon-visualizar.png" width="30" alt="Icone de visualizar" title="Visualizar"></a>
+            <a class="p-2" href="../crud/editar.php?id=<?php echo $um_registro['id_animal']; ?>"><img src="../img/icon-editar.png" width="30" alt="Icone de editar" title="Editar"></a>
+            <a class="p-2" href="../crud/deletar.php?id=<?php echo $um_registro['id_animal']; ?>"><img src="../img/icon-excluir.png" width="30" alt="Icone de excluir" title="Excluir"></a>
+          </td>
+        </tr>
+      </tbody>
   </table>
+<?php
+    endwhile;
+?>
 </div>
 <p class="mb-5 mt-2 text-center fonte-fredoka">
   <a href="../paginas-ong/form-pet.php" role="button" class="cor-btn-blocopets">Novo Cadastro</a>
