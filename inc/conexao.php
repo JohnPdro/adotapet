5ca227e83ef2e22573a05f3b580322a3810e5755
João Pedro <112526011+JohnPdro@users.noreply.github.com>
2023-08-26 16:02:21 -0300

    Corrigindo alguns erros

--- inc/conexao.php
+++ inc/conexao.php
@@ -1,7 +1,9 @@
 <?php
 $bd_usuario = "root";
 $bd_senha = "";
-$bd_servidor = "10.125.47.34";
+$bd_servidor = "localhost";
 $bd_banco = "adotapet";
 $conexao = mysqli_connect($bd_servidor, $bd_usuario, $bd_senha, $bd_banco);
+//10.125.47.34 servidor Gu
 ?>
+
